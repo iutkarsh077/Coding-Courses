9c283c37549ef454383559df57f2a095484fbab6
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-02-29 15:45:50 +0530

    fixes bug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,35 +1,36 @@
-'use client';
+"use client";
 
-import React, { FormEvent, useState } from 'react';
-import { BackgroundBeams } from '@/components/ui/background-beams';
-import { sendEmail } from '@/components/CheckMail';
-import toast, { Toaster } from 'react-hot-toast';
+import React, { FormEvent, useState } from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { sendEmail } from "@/components/CheckMail";
+import toast, { Toaster } from "react-hot-toast";
 
 function CodingSchoolContactUs() {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      console.log('Submitted:', { email, message });
-    sendEmail({email, message});
-    toast.success("Email Sent!", {
-      position: 'bottom-right'
-    })
+      console.log("Submitted:", { email, message });
+      sendEmail({ email, message });
+      toast.success("Email Sent!", {
+        position: "bottom-right",
+      });
+      setEmail("");
+      setMessage("");
     } catch (error) {
       toast.error("Error Occured", {
-        position: 'bottom-right'
-      })
+        position: "bottom-right",
+      });
     }
   };
 
   return (
     <div className="min-h-screen bg-gray-100 ease-in-out dark:bg-gray-900 py-12 pt-36 relative">
-      <Toaster/>
+      <Toaster />
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="max-w-2xl mx-auto p-4 relative z-10">
-       
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
           Contact Us
         </h1>
