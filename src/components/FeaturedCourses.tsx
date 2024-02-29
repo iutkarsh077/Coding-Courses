f011956a7af60936644a49f2df8413c89b55a44b
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-02-29 13:32:50 +0530

    wavy background

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import courseData from "../data/music_courses.json";
+import courseData from "../data/coding_courses.json";
 import { BackgroundGradient } from "./ui/background-gradient";
 import Image from "next/image";
 
