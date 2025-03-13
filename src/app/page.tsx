e0f40516c5f42a48e3297369e0133f6ca5fa95d7
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-03-13 18:22:08 +0530

    fix:email send

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import TestimonialCodeCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
+
 export default function Home() {
   return (
    <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
